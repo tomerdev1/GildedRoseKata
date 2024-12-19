8d0579669e25bc9837b9fc371459af62dde3bcdf
Tomer Abady <tomer.abady@icloud.com>
2024-12-19 17:35:53 +0200

    console log clean up

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -4,7 +4,6 @@ describe("Gilded Rose", () => {
   it("should foo", () => {
     const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    console.log(items);
     expect(items[0].name).toBe("foo");
     expect(items[0].sellIn).toBe(-1);
     expect(items[0].quality).toBe(0);
