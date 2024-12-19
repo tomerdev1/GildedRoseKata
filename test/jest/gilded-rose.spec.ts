6f5ce9dc70dbbe1a39dd9c9f7aaa3486df022f18
Tomer Abady <tomer.abady@icloud.com>
2024-12-19 17:34:44 +0200

    tests cleanup, fix, and run first passing test

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,12 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    console.log(items);
+    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
   });
 });
