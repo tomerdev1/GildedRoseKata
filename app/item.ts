7d4c6d927a9a8c72fb4eff64ca25e32fa904b5f1
Tomer Abady <tomer.abady@icloud.com>
2024-12-20 10:53:33 +0200

    added conjured items business logic

--- app/item.ts
+++ app/item.ts
@@ -2,6 +2,7 @@ export enum SpecialItems {
   AgedBrie = "Aged Brie",
   BackstagePass = "Backstage passes to a TAFKAL80ETC concert",
   Sulfuras = "Sulfuras, Hand of Ragnaros",
+  Conjured = "Conjured",
 }
 
 export class Item {
