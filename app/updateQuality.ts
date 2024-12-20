fb4157419af80468415ab8d1565aa2a3eda1cfb5
Tomer Abady <tomer.abady@icloud.com>
2024-12-20 09:44:06 +0200

    Refactor #3 removing repeating code (especially if statements) from updateQuality methods

--- app/updateQuality.ts
+++ app/updateQuality.ts
@@ -1,30 +1,46 @@
 import { Item } from "./item";
 
-const isQualityIncreasePossible = (item: Item) => item.quality < 50;
-const isQualityDecreasePossible = (item: Item) => item.quality > 0;
+const isQualityAboveMinimum = (quality) => quality > 0;
+const isQualityBelowMaximum = (quality) => quality < 50;
+
+const decreaseQuality = (quality) =>
+  isQualityAboveMinimum(quality) ? quality-- : quality;
+const increaseQuality = (quality) =>
+  isQualityBelowMaximum(quality) ? quality++ : quality;
+
+const updateBasic = (item: Item) => {
+  item.quality = decreaseQuality(item.quality);
+  item.quality =
+    item.sellIn <= 0 ? decreaseQuality(item.quality) : item.quality;
+  return item;
+};
 
 export const updateNormalItem = (item: Item) => {
-  if (isQualityDecreasePossible(item)) item.quality--;
-  item.sellIn--;
-  if (item.sellIn < 0 && isQualityDecreasePossible(item)) item.quality--;
+  item = updateBasic(item);
+  item.sellIn -= 1;
+  return item;
 };
 
 export const updateAgedBrie = (item: Item) => {
-  if (isQualityIncreasePossible(item)) item.quality++;
-  item.sellIn--;
-  if (item.sellIn < 0 && isQualityIncreasePossible(item)) item.quality++;
+  item.quality = increaseQuality(item.quality);
+  item.quality = item.sellIn < 0 ? increaseQuality(item.quality) : item.quality;
+  item.sellIn -= 1;
+  return item;
 };
 
+const increaseBackstagePassesQuality = (item: Item): number => {
+  let quality = increaseQuality(item.quality);
+  quality = item.sellIn < 11 ? increaseQuality(quality) : quality;
+  quality = item.sellIn < 6 ? increaseQuality(quality) : quality;
+  return quality;
+};
 export const updateBackstagePasses = (item: Item) => {
-  if (isQualityIncreasePossible(item)) {
-    item.quality++;
-    if (item.sellIn < 11) item.quality++;
-    if (item.sellIn < 6) item.quality++;
-  }
-  item.sellIn--;
-  if (item.sellIn < 0) {
-    item.quality = 0;
-  }
+  item.quality = item.sellIn <= 0 ? 0 : increaseBackstagePassesQuality(item);
+  item.sellIn -= 1;
+  return item;
 };
 
-export const updateSulfuras = (item: Item) => {};
+export const updateSulfuras = (item: Item) => {
+  item.quality = 80;
+  return item;
+};
